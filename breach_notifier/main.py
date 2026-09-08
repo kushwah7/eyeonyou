18bb16fd1e080bb168920275db437b17feb6acfe
kushwah7 <ankeshkushwaha72@gmail.com>
2026-09-08 12:03:37 +0530

    Updated CORS with Vercel link

--- breach_notifier/main.py
+++ breach_notifier/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://eyeonyou-zeta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
